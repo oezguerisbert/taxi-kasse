260c5b8065c7fa49847b8dad54e5ed91f6dd83b4
Özgür Isbert <oezguerisbert@gmail.com>
2022-01-30 14:53:34 +0100

    fix: trying to fix the pdf creation error

--- app/core/utils/htmlToPDF.ts
+++ app/core/utils/htmlToPDF.ts
@@ -2,6 +2,7 @@ import { renderToStaticMarkup } from "react-dom/server";
 import pdf from "html-pdf";
 export type PageSize = "A4" | "A5";
 export const componentToPDFBuffer = (component, format: PageSize) => {
+  const path = require("path");
   return new Promise((resolve, reject) => {
     const html = renderToStaticMarkup(component);
     pdf
@@ -14,6 +15,7 @@ export const componentToPDFBuffer = (component, format: PageSize) => {
           bottom: "5mm",
           right: "5mm",
         },
+        phantomPath: path.resolve(process.cwd(), "node_modules/phantomjs-prebuilt/lib/phantom/bin/phantomjs"),
         type: "pdf",
         timeout: 30000,
       })
